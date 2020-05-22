4dc634b492ab5090fbf3e32011948fee1bd8ddd1
linjun <linjunli@vt.edu>
2020-05-22 11:48:14 -0700

    Fair Candy Swap

---
+++ LC_FairCandySwap.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fairCandySwap(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        :time comlexity: O(n)
+        :space complexity: O(1)
+        """
+        sum_A = sum(A)
+        sum_B = sum(B)
+        setB = set(B)
+            
+        halfdiff = (sum_B - sum_A) / 2
+            
+        for x in A:
+            if x + halfdiff in set(B):
+                return [x, x + halfdiff]
