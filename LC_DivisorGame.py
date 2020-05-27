a8cd7fd5309e339dc6a6d7e14034f8e69f0c01a5
linjun <linjunli@vt.edu>
2020-05-26 22:01:13 -0700

    Divisor Game

---
+++ LC_DivisorGame.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def divisorGame(self, N):
+        """
+        :type N: int
+        :rtype: bool
+        :time complexity: O(1)
+        :space complexity: O(1)
+        """
+        return N % 2 == 0
+        
