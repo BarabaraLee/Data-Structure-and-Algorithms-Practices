a8c3fa6fd908cfc3ffaefd93fc17c2718f7fe8ca
linjun <linjunli@vt.edu>
2020-06-21 12:51:52 -0700

    fix code of FindPivotIndex.py

--- LC_FindPivotIndex.py
+++ LC_FindPivotIndex.py
@@ -4,34 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         :time complexity: O(n)
-        :space complexity: O(n)
+        :space complexity: O(1)
         """
-        if not nums:
-            return -1
+        S = sum(nums)
+        leftsum = 0
         
-        sumcum = []
-        sumcum.append(nums[0])
-        
-        n = len(nums)
-        
-        for i in range(1, n):
-            sumcum.append(nums[i] + sumcum[i-1]) 
-            
-        for i in range(n):
-            
-            if i == 0:
-                leftsum = 0
-            else:
-                leftsum = sumcum[i - 1]
-                
-            if i == len(nums) - 1:
-                rightsum = 0
-            else: 
-                rightsum = sumcum[n - 1] - nums[i] - sumcum[i-1]
-                
-            if leftsum == rightsum:
+        for i, x in enumerate(nums):
+            if leftsum == S - x - leftsum:
                 return i
-        return -1
-                
-                
+            
+            leftsum += x
         
+        return -1
